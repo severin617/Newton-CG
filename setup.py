de837ab2783795378bd31fb106ccb6be1aeb9e47
Mirco Troue <mirco.troue@gmail.com>
2021-02-17 21:49:19 +0100

    First test build

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="newton_cg",
+    version="0.0.1",
+    author="Severin Reiz",
+    author_email="email@email.de",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/severin617/Newton-CG",
+    packages=setuptools.find_packages(exclude=["tf_slim*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'tensorflow'
+    ],
+)
